67a8d7fe3e8faaab6b2b260c9b62e012df2537de
Doug March <marchdoe@Doug-Marchs-MacBook-Pro.local>
2009-12-03 16:21:10 -0500

    added in authlogic, cleaned up the layout and some other little bits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,20 @@ class ApplicationController < ActionController::Base
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+
+  helper_method :current_user
+
+  private
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
   Jammit::Routes.draw(map)
+  
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
+  
+  map.resources :user_sessions
+  map.resources :users
   map.resources :products
   map.resources :categories
   map.root :products
