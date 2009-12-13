55198bfc8d86873ff5f49d3441737dd0bcfe4dd8
Jack Dempsey <jack.dempsey@gmail.com>
2009-12-13 02:21:03 -0500

    have rake db:seed populate first product with a live price

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
-Product.create(:name => 'Suede university coat', :url => 'http://www.jcrew.com/AST/Browse/MensBrowse/Men_Feature_Assortment/catalogjcrewcomexclusives/sportcoatsouterwear/PRDOVR~10879/99101630642/ENE~1+2+3+22+4294967294+20~~~16~15~all~mode+matchallany~~~~~suede/10879.jsp',
-               :description => 'nice suede coat')
+Product.create(:name => 'Suede university coat', :url => 'http://www.jcrew.com/AST/Browse/MensBrowse/Men_Feature_Assortment/catalogjcrewcomexclusives/sportcoatsouterwear/PRDOVR~10879/99101630642/ENE~1+2+3+22+4294967294+20~~~16~15~all~mode+matchallany~~~~~suede/10879.jsp', :description => 'nice suede coat')
+
+%x{ rake jcrew:fetch_price }
