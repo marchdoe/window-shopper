4e51e8b29f51e80862f99ff9bf63264ff0e1da60
Jack Dempsey <jack.dempsey@gmail.com>
2009-12-13 02:02:01 -0500

    start us off with an example product

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Product.create(:name => 'Suede university coat', :url => 'http://www.jcrew.com/AST/Browse/MensBrowse/Men_Feature_Assortment/catalogjcrewcomexclusives/sportcoatsouterwear/PRDOVR~10879/99101630642/ENE~1+2+3+22+4294967294+20~~~16~15~all~mode+matchallany~~~~~suede/10879.jsp',
+               :description => 'nice suede coat')
