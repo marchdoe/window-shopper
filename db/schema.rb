ccc2b8b4c7e2275953d34b9de08f63e5857c4fd1
Jack Dempsey <jack.dempsey@gmail.com>
2009-12-13 02:01:46 -0500

    add url to product

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091203200630) do
+ActiveRecord::Schema.define(:version => 20091213065745) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20091203200630) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "custom_url"
+    t.string   "url"
   end
 
   create_table "users", :force => true do |t|
